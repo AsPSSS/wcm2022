81afede722757fd41cfb7701c5ccd86f12e629d0
unknown <51033131@gm.nfu.edu.tw>
2022-06-26 15:26:46 +0800

    ROCcc flag

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'First', 'text': '', 'tags': '', 'url': 'First.html'}, {'title': 'about', 'text': '\n \n', 'tags': '', 'url': 'about.html'}, {'title': 'ROC flag 90度反轉', 'text': '', 'tags': '', 'url': 'ROC flag 90度反轉.html'}]};
+var tipuesearch = {"pages": [{'title': 'Brython', 'text': '', 'tags': '', 'url': 'Brython.html'}, {'title': 'UK flag', 'text': '\n \n', 'tags': '', 'url': 'UK flag.html'}, {'title': 'ROC flag 90度反轉', 'text': '', 'tags': '', 'url': 'ROC flag 90度反轉.html'}]};
